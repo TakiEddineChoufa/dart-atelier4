6599200662d780f6164f03d8072e8f53e0d45e95
TakiEddineChoufa <takieddine.choufa@gmail.com>
2025-11-12 12:24:03 -0500

    Delete atelier 3 directory

--- atelier 3/EXI5.dart
+++
@@ -1,7 +0,0 @@
-void creerCompte({String? utilisateur, String? motDePasse}) {
-  print("Utilisateur: $utilisateur, Mot de passe: $motDePasse");
-}
-
-void main() {
-  creerCompte(utilisateur: "Marc", motDePasse: "1234");
-}
--- atelier 3/exo 7.dart
+++
@@ -1,8 +0,0 @@
-void configurerImprimante({bool couleur = true, bool? rectoVerso, int copies = 1}) {
-  print("Couleur: $couleur, RectoVerso: $rectoVerso, Copies: $copies");
-}
-
-void main() {
-  configurerImprimante();
-  configurerImprimante(rectoVerso: true, copies: 3);
-}
